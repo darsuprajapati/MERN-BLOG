4a060f1c7022880d4865e3022d6e4f65987c9a1a
Darshan <darsuprajapati262@gmail.com>
2025-05-14 17:15:58 +0530

    Fix CORS to allow frontend Vercel domain

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL,"https://mern-blog-x6uf.vercel.app"],
     credentials: true
 }))
 
